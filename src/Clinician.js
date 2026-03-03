b93246ed44722ec2a1c2a025964935bbfc25ce97
Jayztrash <jayvion1022@gmail.com>
2026-03-02 22:11:34 -0500

    Add files via upload

---
+++ src/Clinician.js
@@ -0,0 +1,8 @@
+export default function Clinician() {
+  return (
+    <div>
+      <h2>Clinician Dashboard</h2>
+      <p>Welcome, Clinician!</p>
+    </div>
+  );
+}
---
+++ src/Patient.js
@@ -0,0 +1,8 @@
+export default function Patient() {
+  return (
+    <div>
+      <h2>Patient Dashboard</h2>
+      <p>Welcome, Patient!</p>
+    </div>
+  );
+}
