3601da58aa67c83ca1615d37a881b56d46e17783
Jayztrash <jayvion1022@gmail.com>
2026-03-02 22:09:59 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
 import Admin from "./Admin";
+import Patient from "./Patient";
+import Clinician from "./Clinician";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/admin" element={<Admin />} />
+        <Route path="/patient" element={<Patient />} />
+        <Route path="/clinician" element={<Clinician />} />
       </Routes>
     </BrowserRouter>
   );
